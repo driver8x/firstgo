c0be8c7f21f5235f37b3cde6aa81259e68c75aab
driver8 <slackerpatch@gmail.com>
2015-11-16 18:28:50 -0800

    Implemented queue using underlying stack. Stack.Head -> Stack.head

--- gostack/gostack.go
+++ gostack/gostack.go
@@ -1,5 +1,5 @@
 /*
-Package gostack implements a simple stack of integers implemented using a linked list
+Package gostack implements a simple stack of integers implemented using a linked list and a queue of integers using the stack
 */
 package gostack
 
@@ -9,22 +9,33 @@ type node struct {
 }
 
 type Stack struct {
-	Head *node
+	head *node
+}
+
+type EmptyStackError struct {}
+
+func (e *EmptyStackError) Error() string {
+	return "Attempted to pop from an empty stack."
 }
 
 // Push adds in integer to the top of the stack
-func (s *Stack) Push(i int) {
-	s.Head = &node{s.Head, i}
+func (s *Stack) Push(val int) {
+	if s == nil {
+		panic("Attempted operation on uninitialized Stack variable")
+	}
+	s.head = &node{s.head, val}
 }
 
 // Pop removes and returns the integer at the top of the stack
-func (s *Stack) Pop() int {
-	if s.Head == nil {
-		panic("Tried to pop from empty stack")
+func (s *Stack) Pop() (val int, err error) {
+	if s == nil {
+		panic("Attempted operation on uninitialized Stack variable")
+	} else if s.head == nil {
+		return 0, &EmptyStackError{}
 	}
-	temp := s.Head.Val
-	s.Head = s.Head.Next
-	return temp
+	val = s.head.Val
+	s.head = s.head.Next
+	return val, nil
 }
 
 // NewStack creates and returns a new Stack and optionally initializes it with a given list of integers
@@ -36,33 +47,90 @@ func NewStack(input ...int) *Stack {
 	return s
 }
 
+
+// Queue implements a queue of integers using a stack. Dequeue is inefficient.
+type Queue struct {
+	stack *Stack
+}
+
+type EmptyQueueError struct {}
+
+func (e *EmptyQueueError) Error() string {
+	return "Attempted to Dequeue from an empty queue"
+}
+
 // Enqueue adds an int to the back of the queue
-func (s *Stack) Enqueue(i int) {
-	newnode := &node{nil, i}
-	if s.Head == nil {
-		s.Head = newnode
+func (q *Queue) Enqueue(val int) {
+	if q == nil {
+		panic("Attempted operation on uninitialized Queue variable")
+	} else if q.stack == nil {
+		q.stack = NewStack(val)
 	} else {
-		tail := s.Head;
-		for ; tail.Next != nil; tail = tail.Next {}
-		tail.Next = newnode
+		q.stack.Push(val)
 	}
 }
 
 // Dequeue removes and returns the int at the front of the queue
-func (s *Stack) Dequeue() int {
-	if s.Head == nil {
-		panic("Tried to Dequeue from empty queue");
+func (q *Queue) Dequeue() (val int, err error) {
+	if q == nil {
+		panic("Attempted operation on uninitialized Queue variable")
+	} else if q.stack == nil {
+		return 0, &EmptyQueueError{}
 	}
-	val := s.Head.Val
-	s.Head = s.Head.Next
-	return val
+	temp, err := q.stack.Pop()
+	if err != nil {
+		return 0, &EmptyQueueError{}
+	}
+	for {
+		val = temp
+		temp, err = q.stack.Pop()
+		if err == nil {
+			defer q.stack.Push(val)
+		} else {
+			break
+		}
+	}
+	return val, nil
 }
 
 // NewQueue creates and returns a new Queue and optionally initializes it with a given list of integers
-func NewQueue(input ...int) *Stack {
-	q := &Stack{nil}
+func NewQueue(input ...int) (q *Queue) {
+	q = &Queue{}
 	for _, v := range input {
 		q.Enqueue(v)
 	}
 	return q
 }
+
+
+//// Enqueue adds an int to the back of the queue
+//func (s *Stack) Enqueue(val int) {
+//	newnode := &node{nil, val}
+//	if s.Head == nil {
+//		s.Head = newnode
+//	} else {
+//		tail := s.Head
+//		for ; tail.Next != nil; tail = tail.Next {
+//		}
+//		tail.Next = newnode
+//	}
+//}
+//
+//// Dequeue removes and returns the int at the front of the queue
+//func (s *Stack) Dequeue() (val int, err error) {
+//	if s.Head == nil {
+//		return 0, &EmptyStackError{}
+//	}
+//	val = s.Head.Val
+//	s.Head = s.Head.Next
+//	return val, nil
+//}
+//
+//// NewQueue creates and returns a new Queue and optionally initializes it with a given list of integers
+//func NewQueue(input ...int) *Stack {
+//	q := &Stack{nil}
+//	for _, v := range input {
+//		q.Enqueue(v)
+//	}
+//	return q
+//}
--- stackuse/stackuse.go
+++ stackuse/stackuse.go
@@ -2,31 +2,11 @@
 package main
 
 import (
-	"github.com/driver8x/firstgo/gostack"
 	"fmt"
+	"github.com/driver8x/firstgo/gostack"
 )
 
 func main() {
-
-//	s := gostack.NewStack(1)
-//	gostack.Push(s, 2)
-//	gostack.Push(s, 3)
-//	gostack.Push(s, 4)
-//	gostack.Push(s, 5)
-//	fmt.Println(gostack.Pop(s))
-//	gostack.Push(s, 6)
-//	fmt.Println(gostack.Pop(s))
-//	fmt.Println(gostack.Pop(s))
-//	gostack.Push(s, 7)
-//	gostack.Push(s, 8)
-//	gostack.Push(s, 9)
-//	fmt.Println(gostack.Pop(s))
-//	fmt.Println(gostack.Pop(s))
-//	fmt.Println(gostack.Pop(s))
-//	fmt.Println(gostack.Pop(s))
-//	fmt.Println(gostack.Pop(s))
-//	fmt.Println(gostack.Pop(s))
-//	fmt.Println(gostack.Pop(s))
 
 	s := gostack.NewStack(1, 2, 3)
 	s.Push(4)
@@ -43,7 +23,11 @@ func main() {
 	fmt.Println(s.Pop())
 	fmt.Println(s.Pop())
 	fmt.Println(s.Pop())
-	fmt.Println(s.Pop(), "\n\n")
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+
+	fmt.Println("\n\n")
 
 	q := gostack.NewQueue(1, 2, 3)
 	q.Enqueue(4)
@@ -61,6 +45,10 @@ func main() {
 	fmt.Println(q.Dequeue())
 	fmt.Println(q.Dequeue())
 	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+
+	fmt.Println("\n\n")
+
+	fmt.Println(s, q)
 }
-
-
